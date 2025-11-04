a8d1d6ca2faff1202ec3da13338cf00633a82e12
Mr. Rots <rotsutc@gmail.com>
2025-11-04 12:46:09 +0700

    Fix error

--- app_dnn.py
+++ app_dnn.py
@@ -28,7 +28,8 @@ try:
     scaler = joblib.load("scaler.pkl")
     model_loaded = True
 except Exception as e:
-    st.error("❌ Không thể tải mô hình hoặc scaler. Vui lòng kiểm tra file 'DNN_BatchNormalization_bond_strength_model.keras' và 'scaler.pkl'. Chi tiết lỗi: {e}")
+    st.error("❌ Không thể tải mô hình hoặc scaler. Vui lòng kiểm tra file 'DNN_BatchNormalization_bond_strength_model.keras' và 'scaler.pkl'.")
+    st.exception(e)
     model_loaded = False
 
 # =========================
@@ -98,5 +99,6 @@ st.markdown("""
 Toàn bộ dữ liệu chỉ sử dụng cho mục đích học tập và nghiên cứu.
 </div>
 """, unsafe_allow_html=True)
+
 
 
